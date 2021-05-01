e6f49c8720fdba0757ea22deecc67304b2fe6e03
kirill-lukyanov <71383135+kirill-lukyanov@users.noreply.github.com>
2021-05-01 12:45:15 +0300

    Add files via upload

---
+++ lab17Individ/Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿
+namespace lab17Individ
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "part1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(13, 43);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "part2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(323, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(15, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "y";
+            this.label1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.label1_MouseDown);
+            this.label1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.label1_MouseMove);
+            this.label1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.label1_MouseUp);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(423, 152);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(14, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "x";
+            this.label2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.label1_MouseDown);
+            this.label2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.label1_MouseMove);
+            this.label2.MouseUp += new System.Windows.Forms.MouseEventHandler(this.label1_MouseUp);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(323, 123);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(16, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "2";
+            this.label3.MouseDown += new System.Windows.Forms.MouseEventHandler(this.label1_MouseDown);
+            this.label3.MouseMove += new System.Windows.Forms.MouseEventHandler(this.label1_MouseMove);
+            this.label3.MouseUp += new System.Windows.Forms.MouseEventHandler(this.label1_MouseUp);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(240, 175);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(16, 17);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "2";
+            this.label4.MouseDown += new System.Windows.Forms.MouseEventHandler(this.label1_MouseDown);
+            this.label4.MouseMove += new System.Windows.Forms.MouseEventHandler(this.label1_MouseMove);
+            this.label4.MouseUp += new System.Windows.Forms.MouseEventHandler(this.label1_MouseUp);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(392, 186);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(16, 17);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "2";
+            this.label5.MouseDown += new System.Windows.Forms.MouseEventHandler(this.label1_MouseDown);
+            this.label5.MouseMove += new System.Windows.Forms.MouseEventHandler(this.label1_MouseMove);
+            this.label5.MouseUp += new System.Windows.Forms.MouseEventHandler(this.label1_MouseUp);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(322, 248);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(16, 17);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "2";
+            this.label6.MouseDown += new System.Windows.Forms.MouseEventHandler(this.label1_MouseDown);
+            this.label6.MouseMove += new System.Windows.Forms.MouseEventHandler(this.label1_MouseMove);
+            this.label6.MouseUp += new System.Windows.Forms.MouseEventHandler(this.label1_MouseUp);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(291, 175);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(16, 17);
+            this.label7.TabIndex = 2;
+            this.label7.Text = "1";
+            this.label7.MouseDown += new System.Windows.Forms.MouseEventHandler(this.label1_MouseDown);
+            this.label7.MouseMove += new System.Windows.Forms.MouseEventHandler(this.label1_MouseMove);
+            this.label7.MouseUp += new System.Windows.Forms.MouseEventHandler(this.label1_MouseUp);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(349, 210);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(16, 17);
+            this.label8.TabIndex = 2;
+            this.label8.Text = "1";
+            this.label8.MouseDown += new System.Windows.Forms.MouseEventHandler(this.label1_MouseDown);
+            this.label8.MouseMove += new System.Windows.Forms.MouseEventHandler(this.label1_MouseMove);
+            this.label8.MouseUp += new System.Windows.Forms.MouseEventHandler(this.label1_MouseUp);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(322, 186);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(16, 17);
+            this.label9.TabIndex = 2;
+            this.label9.Text = "0";
+            this.label9.MouseDown += new System.Windows.Forms.MouseEventHandler(this.label1_MouseDown);
+            this.label9.MouseMove += new System.Windows.Forms.MouseEventHandler(this.label1_MouseMove);
+            this.label9.MouseUp += new System.Windows.Forms.MouseEventHandler(this.label1_MouseUp);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 74);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Anim On";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(13, 103);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Anim Off";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(782, 753);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint_1);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ lab17Individ/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab17Individ
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            DoubleBuffered = true;
+        }
+        private Point location = new Point(50, 50);
+        private int R = 50;
+        bool DropStarted = false;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Invalidate();
+        }
+        SolidBrush solidBrush1 = new SolidBrush(Color.Red);
+        SolidBrush solidBrush2 = new SolidBrush(Color.Red);
+        Color FillColor1 = Color.Yellow;
+        Color FillColor2 = Color.Yellow;
+        float time = 0;
+        private void Form1_Paint_1(object sender, PaintEventArgs e)
+        {
+            Graphics gr = e.Graphics;
+            BackColor = Color.White;
+            Pen pen = new Pen(Color.Blue, 1);
+            float scale = 0;
+            if (AnimOn)
+            {
+                time++;
+                scale = (float)Math.Sin(time * 0.5) * 10;
+            }
+
+            for (int y = 0; y < Height; y++)
+            {
+                for (int x = 0; x < Width; x++)
+                {
+                    if (BelongsPart1(x, y))
+                    {
+                        gr.FillRectangle(solidBrush1, x, y, 1, 1);
+                    }
+
+                    if (BelongsPart2(x, y))
+                        gr.FillRectangle(solidBrush2, x, y, 1, 1);
+                }
+            }
+            gr.DrawEllipse(pen,-50+Width/2, -50+Height/2, 100, 100);
+            gr.DrawEllipse(pen, -50+Width/2-50, -50+Height/2+50, 100, 100);
+            gr.DrawEllipse(pen, -50+Width/2+50, -50+Height/2+50, 100, 100);
+            gr.DrawEllipse(pen, -100+Width/2, -100+Height/2, 200, 200);
+
+            gr.DrawLine(pen, Width / 2-100, Height / 2+100, Width / 2 +100, Height / 2 - 100);
+            gr.DrawLine(pen, Width / 2, (Height / 2+ 125) + scale, Width / 2, (Height / 2 - 125) - scale);
+            gr.DrawLine(pen, (Width / 2 - 125)-scale, Height / 2, (Width / 2 + 125) + scale, Height / 2);
+        }
+
+        private bool BelongsPart1(int x, int y)
+        {
+            return ((Math.Pow(Width / 2 - x, 2) + Math.Pow(Height / 2 - y, 2)) < Math.Pow(100, 2)) &&
+                        ((Math.Pow(Width / 2 - x, 2) + Math.Pow(Height / 2 - y, 2)) > Math.Pow(50, 2)) &&
+                        ((Math.Pow(Width / 2 - x - 50, 2) + Math.Pow(Height / 2 - y + 50, 2)) < Math.Pow(50, 2)) &&
+                        x+ Height/2 > -y+Width/2+Height;
+        }
+        private bool BelongsPart2(int x, int y)
+        {
+            return (Math.Pow(Width / 2 - x, 2) + Math.Pow(Height / 2 - y, 2)) < Math.Pow(50, 2) &&
+                (Math.Pow(Width / 2 - x+50, 2) + Math.Pow(Height / 2 - y+50, 2)) < Math.Pow(50, 2);
+        }
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            Text = $"X: {e.X} | Y: {e.Y}";
+            if (BelongsPart1(e.X, e.Y)) solidBrush1.Color = FillColor1;
+            else solidBrush1.Color = Color.Red;
+            if (BelongsPart2(e.X, e.Y)) solidBrush2.Color = FillColor2;
+            else solidBrush2.Color = Color.Red;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ColorDialog colorDialog1 = new ColorDialog();
+            if (colorDialog1.ShowDialog() == DialogResult.OK) 
+            {
+                FillColor1 = colorDialog1.Color;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ColorDialog colorDialog2 = new ColorDialog();
+            if (colorDialog2.ShowDialog() == DialogResult.OK)
+            {
+                FillColor2 = colorDialog2.Color;
+            }
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            int curX = e.X, curY = e.Y;
+            bool InX = (curX >= location.X - R && curX <= location.X + R);
+            bool InY = (curY >= location.Y - R && curY <= location.Y + R);
+
+            if (InX && InY)
+                DropStarted = true;
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (DropStarted)
+            {
+                location.X = e.X + R / 2;
+                location.Y = e.Y + R / 2;
+                Invalidate();
+            }
+        }
+        bool isDown;
+        private void label1_MouseDown(object sender, MouseEventArgs e)
+        {
+            isDown = true;
+
+        }
+
+        private void label1_MouseUp(object sender, MouseEventArgs e)
+        {
+            isDown = false;
+        }
+
+        private void label1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDown)
+            {
+                Control c = sender as Control;
+                c.Location = this.PointToClient(Control.MousePosition);
+            }
+
+        }
+        private bool AnimOn;
+        private void button3_Click(object sender, EventArgs e)
+        {
+            AnimOn = true;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            AnimOn = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Point r = new Point()
+        }
+    }
+}
